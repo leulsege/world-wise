a98d49974e5c564ba4b683931905e4010820b145
leul <leulsegedgebremedhin@gmail.com>
2023-12-13 00:42:22 +0300

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={HomePage} />
+        <Route path="/product" element={Product} />
+        <Route path="/pricing" element={Pricing} />
+        <Route path="*" element={PageNotFound} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
