cc884e856e818974a9a580926449651dea1377f3
leul <leulsegedgebremedhin@gmail.com>
2024-01-03 22:55:26 +0300

    world wise

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -16,7 +16,7 @@ export default function Homepage() {
         <h2>
           A world map that tracks your footsteps into every city you can think
           of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+          you have wandered the world. this world is amazing.
         </h2>
         <Link to="/app" className="cta">
           Start tracking now
